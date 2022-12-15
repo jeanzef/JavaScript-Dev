fa7e5d0fa070c9158e886b3c1181a72f37cca6e5
Jean Roberto de Castro Zeferino <jean.zef13@gmail.com>
2022-12-15 01:20:05 -0300

    switch e case

---
+++ aula 9 switch e case e defaut/script.js
@@ -0,0 +1,18 @@
+let permissao;
+permissao = 'diretor';
+switch (permissao){
+    case ('comum'):
+        console.log('usuário comum');
+        break;
+
+    case ('gerente'):
+        console.log('usuário gerente');
+        break;
+
+    case ('diretor'):
+        console.log('usuário diretor');
+        break;
+
+    default:
+    console.log('usuário não reconhecido');
+}
