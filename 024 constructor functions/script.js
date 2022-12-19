e8c9b7f69286c408bf7073326f5d9030204e8710
Jean Roberto de Castro Zeferino <jean.zef13@gmail.com>
2022-12-19 15:24:46 -0300

    ex 12 constructor functions

---
+++ 024 constructor functions/script.js
@@ -0,0 +1,10 @@
+function Celular(marcaCelular,tamanhoTela,capacidadeBateria){
+    this.marcaCelular = marcaCelular,
+    this.tamanhoTela = tamanhoTela,
+    this.capacidadeBateria = capacidadeBateria,
+    this.ligar = function (){
+        console.log('Fazendo Ligação...');
+    }
+}
+const celular = new Celular ('Asus',5.5,5000)
+console.log(celular);
