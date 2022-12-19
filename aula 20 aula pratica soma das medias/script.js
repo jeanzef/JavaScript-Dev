d871e4103c87b851354a1b94562d3f946e205719
Jean Roberto de Castro Zeferino <jean.zef13@gmail.com>
2022-12-19 12:30:44 -0300

    ex 08 soma das medias

---
+++ aula 20 aula pratica soma das medias/script.js
@@ -0,0 +1,14 @@
+const array = [70,70,80];
+console.log(mediaDoAluno(array));
+function mediaDoAluno(notas){
+    let soma = 0;
+    for(let nota of notas){
+        soma += nota;
+    }
+    const media = soma/(notas.length);
+    if(media < 59) return 'F';
+    if(media < 69) return 'D';
+    if(media < 79) return 'C';
+    if(media < 89) return 'B';
+    return 'A';
+}
